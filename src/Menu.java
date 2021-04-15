b973bbacc93056784adfcaec422ef5a8b8548705
jorge <jorge@10.0.2.15>
2021-04-15 17:32:13 +0200

    modificacion apartado 5

--- src/Menu.java
+++ src/Menu.java
@@ -4,6 +4,7 @@ public class Menu {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola este es el proyecto con GitHub");
+		System.out.println("Hola este es el apartado 3");
 	}
 
 }
